1081cee3f2d9bc1c3a77c7cddd6b68c9df645bc4
taddeimania <jtaddei@gmail.com>
2018-05-26 22:14:54 -0400

    Started models for storing contacts and messages, basic UI

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    number = models.CharField(max_length=20)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+
+class Message(models.Model):
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-# Create your views here.
+from .models import Contact
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        if self.request.user.is_authenticated:
+            context['contacts'] = Contact.objects.filter(owner=self.request.user)
+        return context
