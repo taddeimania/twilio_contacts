cf97586bd3b32f18a8bec41b9c849f0a2efe0175
taddeimania <jtaddei@gmail.com>
2018-05-26 21:56:05 -0400

    Remove password validation for demonstration purposes

--- twilio_contacts/settings.py
+++ twilio_contacts/settings.py
@@ -88,20 +88,10 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+
+# REMOVING PASSWORD VALIDATORS FOR DEMONSTRATION PURPOSES ONLY!
+# - Joel Taddei 5-26-2018
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
